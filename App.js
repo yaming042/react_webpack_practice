295c890a52e68c6c80037ab6455535f137e29520
Tim.tian <Tim.tian@mexgroup.com>
2017-06-28 15:26:10 +0800

    a

--- App.js
+++ App.js
@@ -4,6 +4,39 @@ import {render} from 'react-dom';
 import Worklist from './Worklist';
 require("./style.css");
 
+let cardList = [
+	{
+		id: 1,
+		title: 'aaaaaaaaaaaaaaa',
+		description: 'aaaaaaaaaaaaaaaaaaaaaaa',
+		status: 'to_do',
+		tasks: []		
+	},
+	{
+		id: 2,
+		title: 'bbbbbbbbbbbbbbbbb',
+		description: 'bbbbbbbbbbbbbbbbbbbbbbbb',
+		status: 'doing',
+		tasks: [
+			{
+				id: 1,
+				name: 'fddfdfdfd',
+				done: true
+			},
+			{
+				id: 2,
+				name: 'agagagagaga',
+				done: false
+			},
+			{
+				id: 3,
+				name: 'rererererererer',
+				done: false
+			},
+		]		
+	},
+];
+
 class App extends Component {
   render(){
     return (
